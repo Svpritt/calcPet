f01150ad16eace4442b8ebea67f0af2676c61517
Serhii Pritulyak <s.v.pritt@gmail.com>
2023-02-17 14:58:44 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,72 +1,85 @@
 let displayCleared = true; // flag to indicate if the display has been cleared
-let displayNum = "0"; // current display number
-let lastOp = ""; // last operator pressed
 let currOp = ""; // current operator pressed
-let num1 = ""; // first number
-let num2 = ""; // second number
 let total = ""; // result and display output
 let display = document.getElementsByClassName('calc-numbers')[0]; // the calculator display element
-const operators = {
-    '+': function(a, b) { return parseFloat(a) + parseFloat(b) }, // add
-    '-': function(a, b) { return parseFloat(a) - parseFloat(b) }, // subtract я знаю что тут минус но нужен плюс потому что строка с минусом идет перед парс флот!
-    '*': function(a, b) { return parseFloat(a) * parseFloat(b) }, // multiply
-    '/': function(a, b) { if (b === '0') {
-    clearDisplay();
-    display.innerHTML = "Error";   //доречи ця вся хуйня не пашет нихуя
-    return;
+    const ops = {
+        '+': function(aa, bb) { return aa + bb }, // сложение
+        '-': function(aa, bb) { return aa - bb }, // вычитание 
+        '*': function(aa, bb) { return aa * bb }, // multiply
+        '/': function(aa, bb) { return aa / bb }, // division
+        };
+const command = {
+    a: undefined,
+    b: undefined,
+    is_executed: false,
+    operation: function (aa, bb){
+        return aa+bb;
+    },
+    execute: function() {
+        this.is_executed = true;
+        if (this.b === undefined) {
+            this.b = this.a;
+        }
+        total = this.operation(this.a, this.b);
+        this.a = total;
+        return total;
+    },
+    add_num: function (src, num){
+        if (src === undefined){
+            return num;
+        }
+        return src * 10 + num;
+    },
+    add_a_num: function (num) {
+        this.a = this.add_num(this.a, num);
+    },
+    add_b_num: function (num) {
+        this.b = this.add_num(this.b, num);
     }
-    return parseFloat(a) / parseFloat(b)
-    }, // division
-    };
+};
 function clearDisplay(){
+    command.a = undefined;
+    command.b = undefined;
+    command.operation = (aa, bb,) => aa + bb;
+    command.is_executed = false;
     display.innerHTML = "0";
     displayCleared = true;
     total = "";
-    lastOp = "";
-    num1 = "";
-    num2 = "";
-}
-function pressNum(num) {
-    if (displayCleared){
-      num1 = ''+ num;
-      display.innerHTML = num1;
-      displayCleared = false;
-      console.log(num1, num2, lastOp);
-    } else if (num2 === "" && lastOp === ""){
-      num1 += num;
-      display.innerHTML = num1;
-      console.log(num1, num2, lastOp);
-    } else if (num1!=="" && num2!=="" && lastOp ===""){
-      num1 = operators[lastOp](num1, num2);    //это ебаниссимо тоже ниразу в консоле не видел не понятно,
-      console.log(num1, num2, lastOp);
-      display.innerHTML = num1;
-    } else {
-      num2 += num;
-      console.log(num1, num2, lastOp);
-      display.innerHTML = num2;
-    }
-  }
-function pressOp (currOp) {
-    num2 = "";
-    //тут должно быть условие что если я жму оператор не нажав равно, то все считается автоматически и если нажал равно тоже нужно придумать логику
-    
-    displayCleared = false;
-    lastOp = currOp; 
 }
 function equals() {
-    // if (num2 === '') num2 = num1;  //эта дичь должна в случае 5+= показать 10
-    if (num1 !== '' && num2 !== '' && lastOp !== '') {
-      total = operators[lastOp](num1, num2);
-      num1 = total;
-      num1 ="";
-    //   num2 = '';     //ну тут понятно, я обнуляю его перед тем как нажать оператор вернее при нажатии оператора чтоб не было казусов,  а вот ласт оп хз
-    //   lastOp = '';
-      console.log(num1, num2, lastOp);
-    } else if (num1 !== '' && total !== '') {
-      total = operators[lastOp](total, num1);
-      num1 = total;
-      console.log("hello kitty");
-    }
+    total = command.execute();
     display.innerHTML = total;
     displayCleared = true;
   }
+function pressNum(num) {
+    if (displayCleared) {
+        command.add_a_num(num);
+        display.innerHTML = command.a;
+    } else if (displayCleared && command.is_executed) {  // дописал else if вместо if
+        command.is_executed = false;
+        command.a = 0;
+        command.b = 0;
+    }
+     else {
+        command.add_b_num(num);
+        display.innerHTML = command.b;
+    }
+  }
+function pressOp (currOp) {
+    if (displayCleared && command.is_executed) {
+        command.is_executed = false;
+        command.b = 0;
+    }
+    if (displayCleared === false && command.is_executed === false) { //если дисплей не пуст и результат вычислений пуст то
+        equals();
+        displayCleared = false;
+        command.is_executed = false;
+        command.b = 0;
+    }
+    displayCleared = false;
+    command.operation = ops[currOp];
+    if (command.is_executed === true) {
+        total = command.execute();
+        display.innerHTML = total;
+    }
+}
